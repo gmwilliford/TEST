86d3f71b25921f4eeeec4ef82ef85a99592e9bd9
jprker30 <parzivus@gmail.com>
2023-11-14 12:33:04 -0600

    Create Adam_test.R

---
+++ Adam_test.R
@@ -0,0 +1,5 @@
+library(dplyr)
+
+#Calculate species richness: How many species exist in a given area
+#For assignment: Calculate richness for latitudes with moving average (40-41, 40.5-41.5, 41-42, etc)
+#For each latitude, sample 200 random trees and calculate richness 100 times
